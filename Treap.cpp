c654a630c11d05d12973b46a62e8839d90bc1171
chulwoo shin <shinasto@gmail.com>
2017-06-26 10:45:50 +0900

    Update Treap.cpp

--- Treap.cpp
+++ Treap.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
 using namespace std;
 
+unsigned int t1 = 0, t2 = 0;
+unsigned int _rand()
+{
+    unsigned int b;
+    b = t1 ^ (t1 >> 2) ^ (t1 >> 6) ^ (t1 >> 7);
+    t1 = (t1 >> 1) | (~b << 31);
+    b = (t2 << 1) ^ (t2 << 2) ^ (t1 << 3) ^ (t2 << 4);
+    t2 = (t2 << 1) | (~b >> 31);
+    return t1 ^ t2;
+}
+
 template <class T>
 class Node {
 public:
     T key;
     int priority, size;
     Node *left, *right;
-    
-    Node(const T& _key) : key(_key), priority(rand()), size(1), left(NULL), right(NULL) { }
+
+    Node(const T& _key) : key(_key), priority(_rand()), size(1), left(NULL), right(NULL) { }
     void setLeft(Node* newLeft) { left = newLeft; calcSize(); }
     void setRight(Node* newRight) { right = newRight; calcSize(); }
     void calcSize() {
@@ -36,7 +47,7 @@ public:
     int countLessThan(T k) {
         return _countLessThan(_root, k);
     }
-    
+
 private:
     int _countLessThan(Node<T>* root, T& k) {
         if(root==NULL) return 0;
@@ -46,7 +57,7 @@ private:
         int ls = (root->left ? root->left->size : 0);
         return ls + 1 + _countLessThan(root->right, k);
     }
-    
+
     Node<T>* kth(Node<T>* root, int k) {
         int leftSize = 0;
         if(root->left != NULL)
@@ -65,7 +76,7 @@ private:
         a->setRight(merge(a->right, b));
         return a;
     }
-    Node<T>* _erase(Node<T>* root, T& key) {
+    Node<T>* _erase(Node<T>* root, const T& key) {
         if(root == NULL) return root;
         if(root->key == key) {
             Node<T>* ret = merge(root->left, root->right);
@@ -75,7 +86,7 @@ private:
         if(key < root->key) {
             root->setLeft(_erase(root->left, key));
         } else {
-            root->serRight(_erase(root->right, key));
+            root->setRight(_erase(root->right, key));
         }
         return root;
     }
@@ -106,16 +117,3 @@ private:
     }
     Node<T>* _root;
 };
-
-int main(int argc, const char * argv[]) {
-    int arr[5] = {10, 2, 3, 5, 12};
-    
-    TreeMap<int> map;
-    for(int i=0; i<5; i++) {
-        map.insert(arr[i]);
-    }
-    
-    cout << "kth(3): " << map.getKth(3) << endl;
-    cout << "11 less: " << map.countLessThan(11) << endl;
-    return 0;
-}
